bb0aedc33d87d00c09794be1c13c2fcb848ce5a1
jaydwevidi <jayraj1999@gmail.com>
2024-04-22 14:36:00 -0400

    small refactoring

--- backend/controllers/userManagement/editProfile.js
+++ backend/controllers/userManagement/editProfile.js
@@ -1,6 +1,7 @@
 const pool = require("../../config/db");
 const crypto = require("crypto");
 const jwt = require("jsonwebtoken");
+const convertGender = require("../../utils/convertGender");
 
 const bcrypt = require("bcrypt");
 const SECRET_KEY = process.env.JWT_SECRET_KEY;
@@ -38,39 +39,3 @@ exports.editProfile = async (req, res) => {
       .json({ message: "Error editing user", error: error.message });
   }
 };
-
-function convertGender(gender) {
-  // Convert gender to lowercase for case-insensitive comparison
-  const genderLower = gender.toLowerCase();
-
-  // Assign a number to each gender
-  // 1 = Male, 2 = Female, 3 = Non-Binary, 4 = Transgender, 5 = Intersex, 6 = Prefer Not to Say, 7 = Other
-  switch (genderLower) {
-    case "male":
-    case "m":
-      return 1;
-    case "female":
-    case "f":
-      return 2;
-    case "non-binary":
-    case "nonbinary":
-    case "nb":
-      return 3;
-    case "transgender":
-    case "trans":
-    case "tg":
-      return 4;
-    case "intersex":
-    case "i":
-      return 5;
-    case "prefer not to say":
-    case "pnts":
-      return 6;
-    case "other":
-    case "o":
-      return 7;
-    default:
-      // If gender is not recognized, return null
-      return null;
-  }
-}
